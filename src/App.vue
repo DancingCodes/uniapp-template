6c4014b4c4dc42a750ccef37f3e19ce3ba6b0f93
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-02 16:36:57 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup lang="ts">
-import { onLaunch, onShow } from '@dcloudio/uni-app'
-import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
-
-function checkAuth() {
-  const pages = getCurrentPages()
-  const route = pages[pages.length - 1]?.route
-
-  if (!route) return
-
-  if (!userStore.isLoggedIn && route !== 'pages/login/login') {
-    uni.reLaunch({ url: '/pages/login/login' })
-  } else if (userStore.isLoggedIn && route === 'pages/login/login') {
-    uni.reLaunch({ url: '/pages/index/index' })
-  }
-}
-
-onLaunch(checkAuth)
-onShow(checkAuth)
+import { onLaunch } from '@dcloudio/uni-app'
+onLaunch(() => {
+  console.log('App Launch')
+})
 </script>
 <style lang="scss">
 // wot-ui主题样式
