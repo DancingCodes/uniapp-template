aa3803b42c4f322462b06cd05f084211717b9e2e
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-15 11:23:07 +0800

    1

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,9 @@
-import { createApp, createRouter, defineEventHandler, setResponseHeaders } from 'h3'
+import { createApp, createRouter, defineEventHandler } from 'h3'
 import loginHandler from './routes/auth/login.post'
 import meHandler from './routes/auth/me.get'
 import uploadHandler from './routes/files/upload.post'
 
 const app = createApp()
-
-app.use(defineEventHandler((event) => {
-  setResponseHeaders(event, {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*'
-  })
-  if (event.method === 'OPTIONS') return ''
-}))
 
 const router = createRouter()
 router.post('/auth/login', defineEventHandler(loginHandler))
