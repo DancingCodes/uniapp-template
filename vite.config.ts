198dec630f9b662e11f1bf078d0f355085501715
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-15 14:55:01 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3000',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, '')
-  //     }
-  //   }
-  // }
 });
